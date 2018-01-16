abc396d787c317cb394ffec0b138e97d3e4ee226
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2018-01-16 20:36:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name=str(name) + 'loves me'
+	new_name=str(name) + ' we like you'
 	return render_template('index.html', name=new_name)
 
 
